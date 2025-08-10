7bcd5d692d276c2b74da5db2aa85fc804ae7ec56
Fernando Alfaro <139225596+alfarofernando@users.noreply.github.com>
2025-08-09 21:38:53 -0300

    aplicados favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     siteName: "Tu Tienda Online",
     images: [
       {
-        url: "https://lasuerteloca.netlify.app/LaSuerteEsLoca.png", // Cambiar a una imagen representativa real
+        url: "https://lasuerteloca.netlify.app/LaSuerteEsLoca.png",
         width: 1200,
         height: 630,
         alt: "Kits Temáticos para Todas las Ocasiones",
@@ -60,7 +60,24 @@ export const metadata: Metadata = {
   },
 
   manifest: "/site.webmanifest",
+
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", type: "image/x-icon" }
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+    ],
+    other: [
+      { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" }
+    ],
+    shortcut: "/favicon.ico",
+  },
 };
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
